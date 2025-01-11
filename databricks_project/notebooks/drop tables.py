b1e75c39d891882f21d8a9c9dde5f64985975deb
AlexG-Mtl <AlexG-Mtl>
2025-01-11 23:45:48 +0000

    add 1

--- databricks_project/notebooks/drop tables.py
+++ databricks_project/notebooks/drop tables.py
@@ -2,3 +2,8 @@
 # MAGIC %sql
 # MAGIC DROP TABLE IF EXISTS uc_metastore_estus.bronze.incidents_bronze03 ;
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DROP TABLE IF EXISTS uc_metastore_estus.bronze.incidents_bronze03 ;
